6c1d9b817cb7332be07fec2a6e0ec64c45ff28bc
evelyn <126719218+echiewe@users.noreply.github.com>
2026-03-28 17:34:40 -0600

    use client issue on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Background from "./(messages)/components/Background";
 import { useRouter } from "next/navigation";
 
@@ -5,9 +6,9 @@ import { useRouter } from "next/navigation";
 export default function Home() {
     const router = useRouter();
     return (
-        <Background>
-            <h1>Landing page</h1>
-            <button onClick={() => router.push('/login')}>Get started!</button>
+        <Background className="flex flex-col justify-center items-center gap-5">
+            <h1 className="text-2xl">Landing page</h1>
+            <button className='button' onClick={() => router.push('/login')}>Get started!</button>
         </Background>
     );
 }
