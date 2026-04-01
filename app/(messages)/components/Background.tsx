10c23999ed793342bc4dadc2936d77985a360fbb
evelyn <126719218+echiewe@users.noreply.github.com>
2026-04-01 15:15:21 -0600

    conditional header url

--- app/(messages)/components/Background.tsx
+++ app/(messages)/components/Background.tsx
@@ -3,22 +3,26 @@ import NavBar from "./NavBar";
 
 type Props = {
     children: React.ReactNode
+    navBar?: boolean
     className?: string
+    headerIconUrl?: string
     headerTitle?: string
     headerIconFunc?: () => void
 }
 
-export default function Background({ children, className, headerTitle, headerIconFunc }: Props) {
+export default function Background({ children, navBar=true, className, headerIconUrl, headerTitle, headerIconFunc }: Props) {
     return(
         <div className="h-screen w-screen bg-white md:bg-[#ecf0e9] flex justify-center items-center">
             <div className="h-full w-full md:h-4/5 md:w-1/4  md:bg-[#faf9f7] border-dark-green border-4 flex flex-col justify-end items-center">
                 <div className="w-full flex justify-center">
-                    <Header iconUrl='/icons/icon.png' title={headerTitle} onIconClick={headerIconFunc} width={40} />
+                    <Header iconUrl={headerIconUrl || '/icons/icon.png'} title={headerTitle} onIconClick={headerIconFunc} width={40} />
                 </div>
                 <div className={`flex-1 w-full overflow-auto ${className}`}>{children}</div>
+                {navBar &&
                 <div className="w-full">
                     <NavBar />
                 </div>
+                }
             </div>
         </div>
     );
